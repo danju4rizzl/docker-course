4b37682dbcdfe3b04571e1af3cb4857e3d997df5
danju4rizzl <danjumashiwaju@gmail.com>
2024-01-07 20:15:04 +0200

    init commit

---
+++ hello-docker/hello.js
@@ -0,0 +1,3 @@
+// This line of code logs a greeting message to the console. 
+// It's used to verify that the Docker container is running correctly and able to execute JavaScript code.
+console.log('Hello from docker 😃');
